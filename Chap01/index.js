ca9e8ba2cbd51e8468dcdf4a54cd12c9a30e26e4
B H An <anbyunghoon0107@gamil.com>
2021-12-15 23:16:13 +0900

    Variable

--- Chap01/index.js
+++ Chap01/index.js
@@ -1,3 +1,31 @@
 const arr = [1, 2, 3];
 
 arr.forEach(console.log);
+arr.forEach(alert);
+
+/* ---------------------------- variable hoisting --------------------------- */
+console.log(score); // undefined
+var score; // 변수 선언
+score = 80; // 값의 할당
+console.log(score); // 80
+
+console.log(hoisting); // undefined
+var hoisting = 80; // 변수 선언과 값의 할당  => JS엔진이 선언과 할당을 각각 나누어 실행한다.
+console.log(hoisting); // 80
+
+console.log(ask); // undefined
+ask = 123;
+var ask;
+console.log("Ask the Computer : " + ask); // 123
+
+/* ---------------------------- Naming Convention --------------------------- */
+// camelCase
+var firstName;
+// snake_case
+var first_name;
+// PascalCase
+var FirstName;
+// typeHungarianCase
+var strFirstName; // type + identifier
+var $elem = document.getElementById('myId'); // DOM 노드
+var observable$ = fromEvent(document, 'click'); // RxJS 옵저버블
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# JAVASCRIPT
+# ✏ JavaScript
 > JavaScript의 기본 개념과 동작원리를 깊이 있게 학습
+>
+> __Chap01__ :  변수의 개념 및 특성 (네이밍 컨벤션, 호이스팅)
