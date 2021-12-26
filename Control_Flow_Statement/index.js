a94bac1ef29ce5986a1c6d718373ff0880dc85dd
B H An <anbyunghoon0107@gamil.com>
2021-12-26 22:01:58 +0900

    조건문

--- Control_Flow_Statement/index.js
+++ Control_Flow_Statement/index.js
@@ -100,4 +100,146 @@ switch (month) {
     monthName = "Invalid month";
 }
 
-console.log(monthName);
+console.log(monthName); // Invalid month
+
+// switch 문이 끝날 때까지 이후의 모든 case문과 default문을 실행하는 것을 폴스루(fall through)라 한다.
+// 폴스루를 피하기 위해서는 case문에 break문을 넣어준다. break문은 코드 블록에서 탈출하는 역할을 한다.
+
+switch (month) {
+  case 1:
+    monthName = "January";
+    break;
+  case 3:
+    monthName = "March";
+    break;
+  case 7:
+    monthName = "July";
+    break;
+  case 11:
+    monthName = "November";
+    break;
+  default:
+    monthName = "Invalid month";
+}
+
+console.log(monthName); // November
+
+// 일반적으로 default에는 break문을 생략한다.
+var year = 2000;
+var month = 2;
+var days = 0;
+
+switch (month) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    days = 31;
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    days = 30;
+    break;
+  case 2:
+    // 윤년 계산 알고리즘 : 연도가 4로 나누어 떨어지면 윤년이다. 연도가 100으로 나누어 떨어지면 평년, 400은 제외
+    days = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28;
+    break;
+  default:
+    console.log("Invalid month");
+}
+
+console.log(days);
+
+// if...else 문으로 해결할 수 있다면 if...else를 사용하는 것이 좋다.
+
+/* ----------------------------------- 반복문 ---------------------------------- */
+// 반복문은 조건식의 평가 결과가 참인 경우 코드 블록을 실행한다. 그 후 다시 평가하여 거짓일 때 까지 반복한다.
+// for, while, do while문이 있다.
+
+/* ---------------------------------- for문 ---------------------------------- */
+// 조건식이 거짓으로 평가될 때까지 코드블록을 반복 실행한다.
+// 주로 반복을 의미하는 iteration의 i를 사용한다.
+// 변수 선언문은 한 번만 실행된다.
+for (var i = 0; i < 2; i++) {
+  console.log(i);
+} // 0 1
+
+// for문의 변수 선언문, 조건식, 증감식은 모두 옵션이므로 반드시 사용할 필요는 없지만 무한루프가 될 수 있다.
+// for ( ;; ) { ... }
+// for문 내에 for문을 중첩해 사용할 수 있다. 이를 중첩 for문 이라 한다.
+for (var i = 1; i <= 6; i++) {
+  for (var j = 1; j <= 6; j++) {
+    if (i + j === 6) console.log(`[${i}, ${j}]`);
+  }
+}
+
+/* --------------------------------- while문 --------------------------------- */
+// while문은 주어진 조건식의 평가 결과가 참이면 코드블록을 계속해서 반복 실행한다.
+// for문은 반복함수가 명확할 때 주로 사용하고 while문은 불명확할 때 주로 사용한다.
+// 만약 조건식의 평가 결과가 불리언 값이 아니면 강제 변환하여 구별한다.
+var count = 0;
+
+while (count < 3) {
+  console.log(count); // 0 1 2
+  count++;
+}
+
+// if문을 사용해 탈출 조건을 만들고 break문을 넣으면 된다.
+var count = 0;
+while (true) {
+  console.log(count);
+  count++;
+  if (count === 3) break;
+} // 0 1 2
+
+/* ------------------------------- do...while문 ------------------------------ */
+// do...while문은 코드 블록을 먼저 실행하고 조건식을 평가한다.(무조건 한번 이상 실행한다.)
+var count = 0;
+do {
+  console.log(count);
+  count++;
+} while (count < 3);
+
+/* --------------------------------- break문 --------------------------------- */
+// 레이블 문, 반복문, switch문을 탈출한다. 이 외의 break문을 사용하면 SyntaxError(문법에러)가 발생한다.
+/* if (true) {
+  break; // error
+} */
+// 레이블 문이란  식별자가 붙은 문을 말한다. ( foo: console.log() )
+foo: {
+  console.log("foo" + 1);
+  break foo; // foo 레이블 블록문을 탈출한다.
+  console.log("foo" + 2);
+}
+console.log("Done!");
+
+// 중첩된 for문의 내부 for문에서 break문을 사용하면 내부를 탈출하여 외부 for문으로 진입한다.
+// 이 때 내부 for문이 아닌 외부 for문을 탈출하려면 레이블 문을 사용한다.
+outer: for (var i = 0; i < 3; i++) {
+  for (var j = 0; j < 3; j++) {
+    if (i + j === 3) break outer;
+    console.log(`inner [${i}, ${j}]`);
+  }
+}
+console.log("Done!");
+// 레이블문을 사용하면 가독성이 나빠지고 오류를 발생시킬 가능성이 높아지므로 사용을 권장하지 않는다.
+
+/* -------------------------------- continue문 ------------------------------- */
+// continue 문은 반복문의 코드 블록 실행을 현 지점에서 중단하고 반복문의 증감식으로 실행 흐름을 이동 시킨다.
+// 반복문을 탈출하지는 않는다.
+var string = "Hello world!";
+var search = "l";
+var count = 0;
+for (var i = 0; i < string.length; i++) {
+  if (string[i] !== search) continue;
+  count++; // continue 문이 실행되면 이 문은 실행되지 않는다.
+}
+console.log("l의 갯수 : " + count);
+// String.prototype.match 메서드를 사용해도 같은 동작을 한다.
+const regexp = new RegExp(search, "g");
+console.log(string.match(regexp).length); // 3
