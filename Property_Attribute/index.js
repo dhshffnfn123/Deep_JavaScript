807441a829e166f21770c8f519e00232accf6d48
byunghoon An <82787408+dhshffnfn123@users.noreply.github.com>
2022-02-27 23:46:17 +0900

    Update index.js

--- Property_Attribute/index.js
+++ Property_Attribute/index.js
@@ -256,3 +256,32 @@ Object.defineProperties(person, {
 });
 person.fullName = "ByungHoon An";
 console.log(person);
+
+
+//? 객체 변경 방지
+// 객체는 변경 가능 값이므로 재할당 없이 직접 변경할 수 있다. 즉, 프로퍼티를 추가하거나 삭제할 수 있고, 프로퍼티 값을 갱신할 수 있으며, 
+// Object.defineProperty 또는 Object.defineProperties 메서드를 사용하여 프로퍼티 어트리뷰트를 재정의할 수 있다.
+// 자바스크립트는 객체의 변경을 방지하는 다양한 메서드를 제공한다. 객체 변경 방지 메서드들은 객체의 변경을 금지하는 강도가 다르다.
+
+//* 객체 확장 금지
+// Object.preventExtensions 메서든느 객체의 확장을 금지한다. 객체 확장 금지란 프로퍼티 추가 금지를 의미한다.
+// 즉, 확장이 금지된 객체는 프로퍼티 추가가 금지된다.
+// 확장이 가능한 객체인지 여부는 Object.isExtensible 메서드로 확인할 수 있다.
+const person = { name: 'Lee' };
+// person 객체는 확장이 금지된 객체가 아니다.
+console.log(Object.isExtensible(person)); // true
+
+// person 객체의 확장을 금지하여 프로퍼티 추가를 금지한다.
+Object.preventExtensions(person);
+// person 객체는 확장이 금지된 객체다.
+console.log(Object.isExtensible(person)); // false
+// 프로퍼티 추가가 금지된다.
+person.age = 20; // 무시,Strict mode에서는 에러
+console.log(person); // {name: "Lee"}
+// 프로퍼티 추가는 금지되지만 삭제는 가능하다.
+delete person.name;
+console.log(person); // {}
+// 프로퍼티 정의에 의한 프로퍼티 추가도 금지된다.
+Object.defineProperty(person, 'age', { value: 20 });
+//TypeError: Cannot define property age, object is not extensible
+
