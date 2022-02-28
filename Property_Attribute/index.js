cc755e6c625b398c9ab2332e3a17fb688ec87816
byunghoon An <82787408+dhshffnfn123@users.noreply.github.com>
2022-02-28 22:45:54 +0900

    Property_Attribute

--- Property_Attribute/index.js
+++ Property_Attribute/index.js
@@ -285,3 +285,113 @@ console.log(person); // {}
 Object.defineProperty(person, 'age', { value: 20 });
 //TypeError: Cannot define property age, object is not extensible
 
+//? 객체 밀봉
+// Object.seal 메서드는 객체를 말한다. 객체 밀봉이란 프로퍼티 추가 및 삭제와 프로퍼티 어트리뷰트 재정의 금지를 말한다.
+// TODO 밀봉된 객체는 읽기와 쓰기만 가능하다.
+// 밀봉된 객체인지 여부는 Object.isSealed 메서드로 확인할 수 있다.
+
+const person = { name: 'An' };
+// person 객체는 밀봉(seal)된 객체가 아니다.
+console.log(Object.isSealed(person)); // false
+
+// person 객체를 밀봉(Seal)하여 프로퍼티 추가, 삭제, 재정의를 금지한다.
+Object.seal(person);
+
+// person 객체는 밀봉된 객체이다.
+console.log(Object.isSealed(person)); // true
+
+// 밀봉된 객체는 configurable이 false이다.
+console.log(Object.getOwnPropertyDescriptors(person));
+// {  "name": { "value": "An", "writable": true, "enumerable": true, "configurable": false  } }
+
+// 프로퍼티 추가가 금지된다.
+person.age = 20; // 무시, strict mode에서는 에러
+console.log(person); // { name: "An" }
+
+// 프로퍼티 삭제가 금지된다.
+delete person.name; // 무시, strict mode에서는 에러
+console.log(person); // { name: "An" }
+
+// 프로퍼티 값 갱신은 가능하다.
+person.name = "Kim";
+console.log(person);  // { name: "Kim" }
+
+// 프로퍼티 어트리뷰트 재정의가 금지된다.
+Object.defineProperty(person, 'name', { configurable: true }); //! TypeError: Cannot redefine property: name
+
+
+//? 객체 동결
+// Object.freeze 메서드는 객체를 동결한다. 객체 동결이란 프로퍼티 추가 및 삭제와 프로퍼티 어트리뷰트 재정의 금지, 프로퍼티 값 갱신 금지를 의미한다.
+// TODO 즉, 동결된 객체는 읽기만 가능하다.
+// 동결된 객체인지 여부는 Object.isFrozen 메서드로 확인할 수 있다.
+
+const person = { name: 'Lee'};
+
+// person 객체는 동결된 객체가 아니다.
+console.log(Object.isFrozen(person)); // false
+
+// person 객체를 동결하여 프로퍼티의 추가, 삭제, 재정의, 쓰기를 금지한다.
+Object.freeze(person);
+
+// person 객체는 동결된 객체다.
+console.log(Object.isFrozen(person)); // true
+
+// 동결된 객체는 writable과 configurable이 false이다.
+console.log(Object.getOwnPropertyDescriptors(person));
+// {  "name": { "value": "An", "writable": false, "enumerable": true, "configurable": false  } }
+
+// 프로퍼티 추가가 금지된다.
+person.age = 20; // 무시, strict mode에서는 에러
+console.log(person); // { name: "Lee" }
+
+// 프로퍼티 값 갱신이 금지된다.
+person.name = 'An'; // 무시
+console.log(person); // {name: 'Lee'}
+
+// 프로퍼티 어트리뷰트 재정의가 금지된다.
+Object.defineProperty(person, 'name', {configurable: true }); //! TE: Cannot redefine property: name
+
+
+//? 불변 객체
+// 위의 메서드들은 얕은 변경방지(shallow only)로 직속 프로퍼티만 변경이 방지되고 중첩 객체까지는 영향을 주지는 못한다.
+
+const person = {
+	name: 'An'
+	address: { city: 'Seoul' }
+};
+
+// 얕은 객체 동결
+Object.freeze(person);
+// 직속 프로퍼티만 동결한다.
+console.log(Object.isFrozen(person)); // true
+// 중첩 객체까지 동결하지 못한다.
+console.log(Object.isFrozen(person.address)); // false
+
+person.address.city = 'Paju';
+console.log(person); // {name: 'An', address: { city: 'Paju' }}
+
+// 객체의 중첩 객체까지 동결하여 변경이 불가능한 읽기 전용의 불변 객체를 구현하려면 
+// 객체를 값으로 갖는 모든 프로퍼티에 대해 재귀적으로 Object.freeze 메서드를 호출해야한다.
+function deepFreeze(target) {
+	// 객체가 아니거나 동결된 객체는 무시하고 객체이고 동결되지 않은 객체만 동결한다.
+	if(target && typeof target === 'object' && !Object.isFrozen(target)) {
+		Object.freeze(target);
+		Object.keys(target).forEach(key => deepFreeze(target[key]));
+	}
+	return target;
+}
+
+const person = {
+	name: "Lee",
+	address: { city: "Seoul" }
+};
+
+// 깊은 객체 동결
+deepFreeze(person);
+console.log(Object.isFrozen(person)); // true
+// 중첩 객체까지 동결한다.
+console.log(Object.isFrozen(person.address)); // true
+
+person.address.city = "Busan";
+console.log(person); // { name: "Lee", address: { city: "Seoul" }}
+
